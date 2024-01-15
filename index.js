83b3536d899f3c67a28077acee635e8ea7c24f28
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-01-14 17:28:14 -0800

    init commit - initiator class completed

---
+++ index.js
@@ -0,0 +1,12 @@
+const CLI = require('./lib/cli');
+
+const cli = new CLI();
+
+// async function getUserInput() {
+//     const userInput = await cli.run();
+//     console.log(cli);
+// }
+
+// getUserInput();
+
+cli.run();
