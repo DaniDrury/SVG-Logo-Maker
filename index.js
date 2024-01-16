c594c5e1fae34dfbcd38035cd3ab7d8042d822c3
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-01-15 17:22:49 -0800

    further corrected validations by using validate method within inquirer

--- index.js
+++ index.js
@@ -2,11 +2,4 @@ const CLI = require('./lib/cli');
 
 const cli = new CLI();
 
-// async function getUserInput() {
-//     const userInput = await cli.run();
-//     console.log(cli);
-// }
-
-// getUserInput();
-
 cli.run();
